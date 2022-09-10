74efe88e17283a152c7f788f22f55d707fd00387
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2022-09-10 20:52:48 +0530

    Create App.js

---
+++ Destructure_props/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Input from "./components/input";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Input placeholder="enter here" type="text"></Input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
