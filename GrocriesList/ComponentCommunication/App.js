2d1fcafff10d3b78550630aa435f654032012749
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2022-09-11 02:37:00 +0530

    Create App.js

---
+++ GrocriesList/ComponentCommunication/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/item";
+
+//initial state outside;//much better for clear things give you refernece
+const initiList = [
+  { name: "tomato", calorie: 20 },
+  { name: "potato", calorie: 30 },
+  { name: "sugar", calorie: 140 },
+];
+function App() {
+  const [list, setList] = useState(initiList);
+  const removeItemHandle = (e) => {
+    // console.log('i am clicked')
+    //how get i know which event got clicked
+    //dir give more infor them logs.
+    //have property called tareget. Give me which element clicked.
+      //add some property that differetiate.
+    console.dir(e.target.name);
+    //we remove it by names
+    const filteredList = list.filter(v=> v.name !== e.target.name);
+    setList(filteredList);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Groceries List</h2>
+        {list.map((v, i) => {
+          return <Item key={`${i}${v.name}${v.calorie}`} item={v} onClick={removeItemHandle}></Item>;
+        })}
+      </header>
+    </div>
+  );
+}
+
+export default App;
