e2449daa6bb7bfd52aa2b8d3add3b2625be81c78
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2022-09-18 21:52:17 +0530

    Update App.js

--- Testing Philosophy/Unit Test/App.js
+++ Testing Philosophy/Unit Test/App.js
@@ -1,1 +1,15 @@
 
+import './App.css';
+import Button from './Componenets/button/button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button label="click me please" className="button-style"></Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
